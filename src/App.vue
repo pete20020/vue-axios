5619a82ba82662bed67be4fb6a608cc7a82724af
pete20020 <68525430+pete20020@users.noreply.github.com>
2020-09-23 17:47:30 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/download.jpg">
+    <HelloWorld msg=""/>
+    <div>
+</div>
+    <applol/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import applol from './components/applol.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    applol
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/applol.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+      <input type="text" v-model="textSearch"/>
+     <button @click="searchData()">Search</button>
+     {{movielist}}
+     <hr>
+   <!-- <div>
+  <b-card
+    title="Card Title"
+    img-src="https://picsum.photos/600/300/?image=25"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      Some quick example text to build on the card title and make up the bulk of the card's content.
+    </b-card-text>
+     
+    <b-button href="#" variant="primary">Go somewhere</b-button>
+  </b-card> -->
+<!-- </div> -->
+<div>
+  <b-embed
+    type="iframe"
+    aspect="16by9"
+    src="https://www.youtube.com/embed/wBcBzJ_oofs"
+    allowfullscreen
+  ></b-embed>
+</div>
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+export default {
+    data(){
+        return{
+          movielist:null,
+          texeSearch:'',
+        }
+    },
+     mounted:{
+        searchData(){
+        axios
+      .get('https://movie-database-imdb-alternative.p.rapidapi.com'+this.texeSearch+'&limit=100')
+      .then(response => {
+        this.movielist = response.data.results
+      })
+      .catch(error => {
+        console.log(error)
+      })
+       
+    }
+     } 
+}
+</script>
+  
+<style>
+
+</style>
